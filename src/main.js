cc06b2765149fc9c9dc49b3d1fda05613983a880
Christian <cjlaborde@gmail.com>
2020-09-25 05:49:01 -0400

    Protect against users using fake token to login

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './vuex/store'
+import axios from 'axios'
 
 Vue.config.productionTip = false
 
@@ -16,6 +17,20 @@ new Vue({
       const userData = JSON.parse(userString)
       this.$store.commit('SET_USER_DATA', userData)
     }
+    // allow us to intercept axios request
+    axios.interceptors.response.use(
+      response => response,
+      error => {
+        // Meaning user doesn't have proper credentials to access
+        // The page they trying to
+        if (error.response.status === 401) {
+          this.$store.dispatch('logout')
+        }
+        // Creates a new rejected promise for the provided reason
+        // we returning a rejected promise as the error been the reason
+        return Promise.reject(error)
+      }
+    )
   },
   render: h => h(App)
 }).$mount('#app')
