8993e577bcc503c15e7ce3db4a4c30cff5ee4b24
Christian <cjlaborde@gmail.com>
2020-09-25 05:12:05 -0400

    Automatic Login

--- src/main.js
+++ src/main.js
@@ -8,5 +8,14 @@ Vue.config.productionTip = false
 new Vue({
   router,
   store,
+  // code to run when our app created
+  // This will be used to automatic login
+  created() {
+    const userString = localStorage.getItem('user')
+    if (userString) {
+      const userData = JSON.parse(userString)
+      this.$store.commit('SET_USER_DATA', userData)
+    }
+  },
   render: h => h(App)
 }).$mount('#app')
